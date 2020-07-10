fc054e866ac552ba6fd86e5cf674d749b6a7b43d
Silas Henrique <silash35@gmail.com>
2020-07-10 15:46:49 -0300

    Adcionado Loop em outra thread

--- totem/main.py
+++ totem/main.py
@@ -1,6 +1,15 @@
+from threading import Thread
 from kivy.app import App
 from interface import *
 from paciente import *
+
+#Codigo que irá rodar em loop em segundo plano
+def runA():
+    while(True):
+        paciente.getDados()
+t1 = Thread(target = runA)
+t1.setDaemon(True)
+t1.start()
 
 class Totem(App):
     def build(self):
