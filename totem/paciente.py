ef01d0e1403f93807c104b202172a25561f92b89
Silas Henrique <silash35@gmail.com>
2020-07-10 12:03:03 -0300

    Implementei a classe paciente

---
+++ totem/paciente.py
@@ -0,0 +1,59 @@
+#Constantes medicas, só devem ser alteradas 1 vez
+temperaturaMax = 0
+saturacaoMin = 0
+pressaoMin = 0
+pressaoMax = 0
+
+class Paciente:
+
+    def __init__(self):
+        self.temperatura = 0
+        self.pressao = 0
+        self.saturacao = 0
+        # Não tem maneira melhor,e mais escalável de calcular o grau de emergencia do que com pontos
+        self.pontosDePerguntas = 0
+
+    def zeraDados(self):
+        self.temperatura = 0
+        self.pressao = 0
+        self.saturacao = 0
+        self.pontosDePerguntas = 0
+
+    def getDados(self):
+        #codigo leitura da GPIO
+
+        # Como essa função vai rodar em Loop, 
+        # talves ela pegue dados errados da GPIO, e não 
+        # queremos que ela sobrescreva os dados antigos
+        # caso os dados antigos sejam mais "graves"
+        temp = 0
+        press = 0 
+        satura = 0
+        if(temp > self.temperatura):
+            self.temperatura = temp
+        if(press > self.pressao):
+            self.pressao = press
+        if(satura < self.saturacao):
+            self.saturacao = satura
+
+    def pontosTotal(self):
+        #codigo calcular total de pontos
+        ptemp = 0
+        if(self.temperatura > temperaturaMax):
+            ptemp = 10
+
+        ppress = 0
+        if(self.pressao > pressaoMax or self.pressao < pressaoMin):
+            ppress = 10
+
+        psatura = 0
+        if(self.saturacao < saturacaoMin):
+            psatura = 10
+
+        return (self.pontosDePerguntas + ptemp + ppress + psatura)
+
+    def enviarEmail(self):
+        #codigo enviar resultado para Email, chamando 'pontosTotal'
+        pass
+
+paciente = Paciente()
