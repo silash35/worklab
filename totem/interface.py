5352741059a4498548b8355f7d9b542e4af4134e
Silas Henrique <silash35@gmail.com>
2020-07-15 16:48:37 -0300

    Bug misterioso concertado

--- totem/interface.py
+++ totem/interface.py
@@ -1,12 +1,15 @@
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+
 from kivy.graphics.context_instructions import Color
+from kivy.graphics.instructions import InstructionGroup
 from kivy.graphics.vertex_instructions import Rectangle
-from kivy.graphics import *
+
 from kivy.utils import get_color_from_hex
 from paciente import *
 
 nTelas = 10 #numero de telas atual (começa a contagem do 0)
+primeiraVez = True #Solução temporaria pra o bug misterioso
 
 Builder.load_file('interface.kv')
 class Interface(ScreenManager):
@@ -21,23 +24,46 @@ class Interface(ScreenManager):
         else:
             self.current = '0'
 
+        
+        global primeiraVez
+        if(primeiraVez):
+            self.current = str(nTelas)
+            paciente.pontosDePerguntas = 1
+
         if(paciente.pontosDePerguntas != 0):
+
+            self.current = str(nTelas)
 
             self.ids['result'].color = [1,1,1,1]
             self.ids['result'].text = str(calculaGrau(paciente.pontosTotal())[0])
             #Desenha retângulo com a cor dependendo do grau de emergência.
-            #Aqui tem um Bug que ele só desenha na segunda vez que o programa roda, não consegui resolver
-            with self.ids['result'].canvas.before:
+            corGrau = InstructionGroup()
+            corGrau.add(
                 Color(
                     get_color_from_hex(calculaGrau(paciente.pontosTotal())[1])[0],
                     get_color_from_hex(calculaGrau(paciente.pontosTotal())[1])[1],
                     get_color_from_hex(calculaGrau(paciente.pontosTotal())[1])[2],
-                    get_color_from_hex(calculaGrau(paciente.pontosTotal())[1])[3],
-                    )
-                Rectangle(pos = self.ids['result'].pos, size = self.ids['result'].size)
+                    get_color_from_hex(calculaGrau(paciente.pontosTotal())[1])[3]
+                )
+            )
+            corGrau.add(
+                Rectangle(
+                    pos=self.ids['result'].pos,
+                    size=self.ids['result'].size
+                )
+            )
+            self.ids['result'].canvas.before.add(corGrau)
+            
+            if(primeiraVez):
+                paciente.id = -1
+                paciente.zeraDados()
+            else:
+                paciente.publicarNoSite()
 
-            self.current = str(nTelas)
-            paciente.publicarNoSite()
+        if(primeiraVez):
+            primeiraVez = False
+            self.current = '1'
+            
 
     def addPontos(self, pontos):
         paciente.pontosDePerguntas = int(paciente.pontosDePerguntas) + int(pontos)
