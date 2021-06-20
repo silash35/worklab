46a4fb551b2ea201d52fa0cea1ab43f0491a51da
Silas Henrique <silash35@gmail.com>
2021-06-20 17:15:39 -0300

    start to develop green indentification

--- OBR/2021/out/main.cs
+++ OBR/2021/out/main.cs
@@ -36,9 +36,26 @@ void Main() {
     float saida = pid.calcular(bc.Lightness(0),bc.Lightness(1));
 
     if (saida != 0) {
-        bc.MoveFrontal(saida, -saida);
+      bc.MoveFrontal(saida, -saida);
     }else {
       bc.MoveFrontal((float)80, (float)80);
+    }
+
+    bc.PrintConsole(0, bc.ReturnColor(0));
+    bc.PrintConsole(1, bc.ReturnColor(1));
+
+    if(bc.ReturnColor(0) == "GREEN"){
+      bc.MoveFrontal((float)100, (float)100);
+      bc.Wait(1000);
+
+      bc.MoveFrontal((float)-100, (float)100);
+      bc.Wait(15000);
+    }else if(bc.ReturnColor(1) == "GREEN"){
+      bc.MoveFrontal((float)100, (float)100);
+      bc.Wait(1000);
+
+      bc.MoveFrontal((float)100, (float)-100);
+      bc.Wait(15000);
     }
   }
 }
