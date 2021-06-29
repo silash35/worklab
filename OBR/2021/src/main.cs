eee15f37f27f011d3adeaa559105cf36d0234bef
Silas Henrique <silash35@gmail.com>
2021-06-29 18:00:52 -0300

    start to develop rescue code

--- OBR/2021/src/main.cs
+++ OBR/2021/src/main.cs
@@ -4,7 +4,11 @@ void Main() {
   PID pid = new PID();
   bc.TurnLedOn(255, 255, 255);
 
-  while (true) {
+  while (bc.Distance(0)>5) {
     seguirLinha();
+  }
+
+  while(true){
+    // Código do resgate
   }
 }
