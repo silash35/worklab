aee8199a9543333a6f89df960209f2814ecc6a4b
Silas Henrique <silash35@gmail.com>
2022-10-16 18:02:23 -0300

    add section class

--- UFBA/WMS/WMS.ino
+++ UFBA/WMS/WMS.ino
@@ -1,63 +1,68 @@
+#include "section.hpp"
+
 #define LED_GREEN 5
 #define LED_YELLOW 6
 #define LED_RED 7
 
-#define PIN_TRIG A1
-#define PIN_ECHO A0
-
-#define PIN_PIEZO 9
-
-#define EMPTY_BOX_VALUE 335
-#define FULL_BOX_VALUE 4
-#define MAX_BOX_PERCENTAGE 90
-
-long readUltrasonicDistance(int triggerPin, int echoPin) {
-  pinMode(triggerPin, OUTPUT); // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return 0.01723 * pulseIn(echoPin, HIGH);
-}
+Section sections[] = {Section(A1, A0)};
+bool alreadyWarned = false;
 
 void setup() {
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 
-  pinMode(PIN_TRIG, OUTPUT);
-  pinMode(PIN_ECHO, INPUT);
-
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
-  int distance = readUltrasonicDistance(PIN_TRIG, PIN_ECHO);
-  int porcentage = map(distance, EMPTY_BOX_VALUE, FULL_BOX_VALUE, 0, 100);
 
-  if (porcentage >= MAX_BOX_PERCENTAGE) {
-    tone(PIN_PIEZO, porcentage * porcentage);
+  bool containerEmpty = true;
+  bool containerHalf = true;
+  bool containerFull = true;
 
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_YELLOW, LOW);
-    digitalWrite(LED_RED, HIGH);
-  } else if (porcentage >= 50) {
-    noTone(PIN_PIEZO);
+  bool callTruck = true;
 
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_YELLOW, HIGH);
-    digitalWrite(LED_RED, LOW);
-  } else {
-    noTone(PIN_PIEZO);
+  for (int i = 0; i < sizeof(sections) / sizeof(sections[0]); i++) {
+    int state = sections[i].getState();
 
+    if (state == 2) {
+      Serial.println("mat: Seção " + String(i) + " Cheia");
+    }
+
+    if (state != 0) {
+      containerEmpty = false;
+    }
+
+    if (state < 2) {
+      containerFull = false;
+    }
+  }
+
+  if (containerEmpty || containerFull) {
+    containerHalf = false;
+  }
+
+  if (containerEmpty) {
     digitalWrite(LED_GREEN, HIGH);
     digitalWrite(LED_YELLOW, LOW);
     digitalWrite(LED_RED, LOW);
-  }
 
-  Serial.println(porcentage);
+    alreadyWarned = false;
+  } else if (containerHalf) {
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_YELLOW, HIGH);
+    digitalWrite(LED_RED, LOW);
+
+    alreadyWarned = false;
+  } else if (containerFull) {
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_RED, HIGH);
+
+    if (!alreadyWarned) {
+      Serial.println("truck: Container Cheio");
+      alreadyWarned = true;
+    }
+  }
 }
---
+++ UFBA/WMS/section.hpp
@@ -0,0 +1,59 @@
+#define EMPTY_BOX_VALUE 335
+#define FULL_BOX_VALUE 4
+#define MAX_BOX_PERCENTAGE 90
+
+long readUltrasonicDistance(int triggerPin, int echoPin) {
+  pinMode(triggerPin, OUTPUT); // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return 0.01723 * pulseIn(echoPin, HIGH);
+}
+
+// Class to control a section of a container
+class Section {
+private:
+  int pinTrigger = 0;
+  int pinEcho = 0;
+
+  bool alreadyWarned = false;
+
+public:
+  Section(int pt, int pe) {
+    pinMode(pt, OUTPUT);
+    pinMode(pe, INPUT);
+
+    pinTrigger = pt;
+    pinEcho = pe;
+  }
+
+  int getState() {
+    /*
+      0: Empty
+      1: Half
+      2: Full (And needs to notify the mat operator)
+      3: Full (And the mat operator was already notified)
+    */
+
+    int distance = readUltrasonicDistance(pinTrigger, pinEcho);
+    int percentage = map(distance, EMPTY_BOX_VALUE, FULL_BOX_VALUE, 0, 100);
+
+    if (percentage >= MAX_BOX_PERCENTAGE) {
+      if (alreadyWarned) {
+        return 3;
+      }
+      return 2;
+    } else if (percentage >= 50) {
+      alreadyWarned = false;
+      return 1;
+    } else {
+      alreadyWarned = false;
+      return 0;
+    }
+  }
+};
