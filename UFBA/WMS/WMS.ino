fab706d6045440dbda074120ec6c701ffcebbe5b
Silas Henrique <silash35@gmail.com>
2022-11-02 19:45:10 -0300

    add first line comment on WMS.ino

--- UFBA/WMS/WMS.ino
+++ UFBA/WMS/WMS.ino
@@ -1,3 +1,13 @@
+/*
+  Waste Monitoring System (WMS)
+  Sistema de monitoramento de resíduos
+
+  Trabalho de Introdução a Engenharia de controle e automação da Equipe 3.
+  Instruções:
+  1- Fazer o upload desse código para o Arduino
+  2- Executar o daemon com o comando "pnpm start" na pasta ./daemon
+*/
+
 #include "container.hpp"
 
 // Iniciar objeto container
