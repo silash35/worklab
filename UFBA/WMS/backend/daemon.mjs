9c66aeaa6008070e969fa1540395a930353978bc
Silas Henrique <silash35@gmail.com>
2022-11-02 19:07:33 -0300

    add comments to backend code

--- UFBA/WMS/backend/daemon.mjs
+++ UFBA/WMS/backend/daemon.mjs
@@ -3,17 +3,21 @@ import nodemailer from "nodemailer";
 import { ReadlineParser, SerialPort } from "serialport";
 dotenv.config();
 
+// Iniciar porta serial
 const port = new SerialPort({
   path: "/dev/ttyUSB0",
   baudRate: 115200,
 });
 
+// Configurar porta para ler cada linha separadamente
 const parser = port.pipe(new ReadlineParser("\n"));
 
+// Imprimir na tela sempre que a porta serial conectar
 port.on("open", () => {
   console.log("Porta Serial Aberta (Arduíno conectado com sucesso)");
 });
 
+// Criar transporter para envio de emails usando variáveis de ambiente.
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: 587,
@@ -24,12 +28,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-parser.on("data", (data) => {
-  console.log("[Arduino] ", data);
-
-  let text = data;
+// Executar rotina sempre que dados novos chegarem no serial
+parser.on("data", (text) => {
+  // Imprimir na tela mensagem recebida
+  console.log("[Arduino] ", text);
 
   if (text.startsWith("mat:")) {
+    // Se a mensagem for pro operador da esteira industrial (mat)
     transporter
       .sendMail({
         from: `"Arduino" <${process.env.EMAIL_USER}>`,
@@ -43,9 +48,8 @@ parser.on("data", (data) => {
       .catch(() => {
         console.log("Um Erro ocorreu ao enviar Email para o operador da esteira");
       });
-  }
-
-  if (text.startsWith("truck: ")) {
+  } else if (text.startsWith("truck: ")) {
+    // Se a mensagem for pro operador do caminhão (truck)
     transporter
       .sendMail({
         from: `"Arduino" <${process.env.EMAIL_USER}>`,
