5eb2d6e90d59aa6a50a280a35e43361cb82aad30
Silas Henrique <silash35@gmail.com>
2024-03-21 15:55:16 -0300

    add poetry.toml

--- UFBA/ENGG03/engg03/main.py
+++ UFBA/ENGG03/engg03/main.py
@@ -1,11 +1,27 @@
-import sys
 import numpy as np
 
-baseList = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=np.float64)
+# Criando a matriz dos coeficientes
+A = np.array(
+    [
+        [np.sin(1.0), np.cos(1.0), np.sin(2.0 * 1.0), np.cos(2.0 * 1.0)],
+        [np.sin(2.0), np.cos(2.0), np.sin(2.0 * 2.0), np.cos(2.0 * 2.0)],
+        [np.sin(3.0), np.cos(3.0), np.sin(2.0 * 3.0), np.cos(2.0 * 3.0)],
+        [np.sin(4.0), np.cos(4.0), np.sin(2.0 * 4.0), np.cos(2.0 * 4.0)],
+    ]
+)
 
-showList = baseList
-showList = baseList / 9
+# Criando o vetor das constantes
+b = np.array([[1.0], [2.0], [3.0], [4.0]])
 
-hello = "".join(str(x) + ", " for x in showList)
+# Resolvendo o sistema linear
+sol = np.linalg.solve(A, b)
 
-print(hello)
+# Imprimindo a solução
+print("A1 =", sol[0, 0])
+print("B1 =", sol[1, 0])
+print("A2 =", sol[2, 0])
+print("B2 =", sol[3, 0])
+
+# Validando a solução
+# Validando a resposta
+print(A.dot(sol))
