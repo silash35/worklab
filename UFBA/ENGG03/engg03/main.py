2c31c4f40891024a1ea689ccc744b86f6d1a6436
Silas Henrique <silash35@gmail.com>
2024-03-22 13:34:16 -0300

    remove zeros

--- UFBA/ENGG03/engg03/main.py
+++ UFBA/ENGG03/engg03/main.py
@@ -3,15 +3,15 @@ import numpy as np
 # Criando a matriz dos coeficientes
 A = np.array(
     [
-        [np.sin(1.0), np.cos(1.0), np.sin(2.0 * 1.0), np.cos(2.0 * 1.0)],
-        [np.sin(2.0), np.cos(2.0), np.sin(2.0 * 2.0), np.cos(2.0 * 2.0)],
-        [np.sin(3.0), np.cos(3.0), np.sin(2.0 * 3.0), np.cos(2.0 * 3.0)],
-        [np.sin(4.0), np.cos(4.0), np.sin(2.0 * 4.0), np.cos(2.0 * 4.0)],
+        [np.sin(1), np.cos(1), np.sin(2 * 1), np.cos(2 * 1)],
+        [np.sin(2), np.cos(2), np.sin(2 * 2), np.cos(2 * 2)],
+        [np.sin(3), np.cos(3), np.sin(2 * 3), np.cos(2 * 3)],
+        [np.sin(4), np.cos(4), np.sin(2 * 4), np.cos(2 * 4)],
     ]
 )
 
 # Criando o vetor das constantes
-b = np.array([[1.0], [2.0], [3.0], [4.0]])
+b = np.array([[1], [2], [3], [4]])
 
 # Resolvendo o sistema linear
 sol = np.linalg.solve(A, b)
