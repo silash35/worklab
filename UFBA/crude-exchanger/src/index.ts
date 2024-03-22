15e4939c4ef9c72a70b0efd9bdc794b7ffe2ef0f
Silas Henrique <silash35@gmail.com>
2024-03-22 13:52:46 -0300

    add crude-exchanger

---
+++ UFBA/crude-exchanger/src/index.ts
@@ -0,0 +1,46 @@
+function hasRepeatedDigits(text: string) {
+  const digits = text.split("");
+  const seen = new Set();
+  for (const digit of digits) {
+    if (seen.has(digit)) return true;
+    seen.add(digit);
+  }
+  return false;
+}
+
+// Create an array with all the possible combinations (0 to 5555)
+let array = Array.from({ length: 5555 }, (_, i) => i);
+
+// Filter the combinations that do not match the requirements of the problem
+array = array.filter((item) => {
+  const sItem = String(item);
+
+  // The combination must not contain any of the numbers 6, 7, 8 or 9. Only the digits 0, 1, 2, 3, 4, 5 are allowed
+  if (
+    sItem.includes("6") ||
+    sItem.includes("7") ||
+    sItem.includes("8") ||
+    sItem.includes("9")
+  ) {
+    return false;
+  }
+
+  // If the combination is not divisible by 5, it is not valid
+  if (item % 5 !== 0) {
+    return false;
+  }
+
+  // The combination must have exactly 4 digits
+  if (sItem.length !== 4) {
+    return false;
+  }
+
+  // The combination must not contain any repeated digits
+  if (hasRepeatedDigits(sItem)) {
+    return false;
+  }
+
+  return true;
+});
+
+console.log(array.length);
