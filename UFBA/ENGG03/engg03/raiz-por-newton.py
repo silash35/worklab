936d332e9790b77862d83f0848c08c23343c4fd6
Silas Henrique <silash35@gmail.com>
2024-03-26 16:00:00 -0300

    add pyright

--- UFBA/ENGG03/engg03/raiz-por-newton.py
+++ UFBA/ENGG03/engg03/raiz-por-newton.py
@@ -1,29 +1,41 @@
 import sympy as sp
+from typing import cast
 
 x: sp.Symbol = sp.symbols("x")
 
 
-def newton(F: sp.Expr, x0: float, tol: float = 0.00000000001) -> float:
-    maxIter = 100
+def printFunção(f: sp.Expr):
+    print("Função:")
+    print(sp.latex(f))
 
-    f: sp.Expr = sp.diff(F, x)
+
+def newton(f: sp.Expr, x0: float, tol=0.00000000001):
+    df = sp.diff(f, x)
+
+    def função(xValue: float):
+        return cast(float, f.evalf(subs={x: xValue}))
+
+    def derivada(xValue: float):
+        return cast(float, df.evalf(subs={x: xValue}))
+
+    maxIter = 100
     for i in range(maxIter):
         print("x" + str(i) + ":", x0)
-        x1: float = x0 - F.evalf(subs={x: x0}) / f.evalf(subs={x: x0})
-        if abs(x1 - x0) < tol:
-            return x1
+        x1: float = x0 - (função(x0) / derivada(x0))
         x0 = x1
+        if abs(função(x1)) < tol:
+            return x1
     raise Exception("Falha ao encontrar a raiz")
 
 
 print("E 3.4.1:")
-F = sp.cos(x) - x**2
-print("Função:", F)
+F = sp.sympify("cos(x)-x**2")
+printFunção(F)
 print("{:.5f}".format(newton(F, 1)))
 print()
 
 print("E 3.4.3:")
-F = sp.exp(-(x**2)) - x
-print("Função:", F)
+F = sp.sympify("exp(-(x**2)) - x")
+printFunção(F)
 print("{:.8f}".format(newton(F, 1)))
 print()
