13c227380dcda4c3fd473f347de735e2d03aaa14
Silas Henrique <silash35@gmail.com>
2024-03-28 13:26:44 -0300

    add visualize.py

--- UFBA/ENGG03/engg03/raiz-por-newton2.py
+++ UFBA/ENGG03/engg03/raiz-por-newton2.py
@@ -1,39 +1,41 @@
-import numpy as np
+import sympy as sp
 from scipy.optimize import fsolve
 
+x: sp.Symbol = sp.symbols("x")
 
 # Questão 1: Projeto de circuito
-def q(x: float):
-    t = 0.05
-    C = 10**-4
-    L = 5
-    return (
-        np.exp((x * t) / (2 * L))
-        * np.cos(t * np.sqrt((1 / (L * C)) - (x / (2 * L)) ** 2))
-        - 0.01
-    )
+t = 0.05
+C = 10**-4
+L = 5
+funçãoAnalítica = (
+    sp.exp((x * t) / (2 * L)) * sp.cos(t * sp.sqrt((1 / (L * C)) - (x / (2 * L)) ** 2))
+    - 0.01
+)
 
-
+# Solução
 print("Projeto de circuito:")
+q = sp.lambdify(x, funçãoAnalítica, "numpy")  # Conversão para função numérica
 raiz = fsolve(q, 300)
 print("A raiz da função:", raiz)
+print("Teste da raiz:", q(raiz))
 print()
 
 
 # Questão 2: Fator de Fricção
-def colebrook(x: float):
-    rho = 1.23
-    mu = 1.79e-5
-    D = 0.005
-    epsilon = 1.5e-6
-    v = 40
-    Re = (rho * v * D) / mu
-    return -2 * np.log10((epsilon / (3.7 * D)) + (2.51 / (Re * np.sqrt(x)))) - (
-        1 / np.sqrt(x)
-    )
+rho = 1.23
+mu = 1.79e-5
+D = 0.005
+epsilon = 1.5e-6
+v = 40
+Re = (rho * v * D) / mu
+funçãoAnalítica = 2 * sp.log((epsilon / (3.7 * D)) + (2.51 / (Re * sp.sqrt(x))), 10) + (
+    1 / sp.sqrt(x)
+)
 
-
+# Solução
 print("Fator de Fricção:")
-raiz = fsolve(colebrook, 0.01)
+colebrook = sp.lambdify(x, funçãoAnalítica, "numpy")
+raiz = fsolve(colebrook, 0.01)  # Conversão para função numérica
 print("A raiz da função:", raiz)
+print("Teste da raiz:", colebrook(raiz))
 print()
---
+++ UFBA/ENGG03/engg03/visualize.py
@@ -0,0 +1,33 @@
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+x: sp.Symbol = sp.symbols("x")
+
+# Definição da função
+t = 0.05
+C = 10**-4
+L = 5
+funçãoAnalítica = (
+    sp.exp((x * t) / (2 * L)) * sp.cos(t * sp.sqrt((1 / (L * C)) - (x / (2 * L)) ** 2))
+    - 0.01
+)
+
+# Gera o arquivo latex.md
+markdown = open("latex.md", "w")
+markdown.write("# Função:\n\n")
+markdown.write(f"$${sp.latex(funçãoAnalítica)}$$")
+markdown.close()
+
+# Gera função numérica
+funçãoNumérica = sp.lambdify(x, funçãoAnalítica, "numpy")
+
+# Cria Vetor com possíveis valores de x para teste da função
+vX = np.linspace(300, 350, 100)  # Start, End, Number of Points
+vY = funçãoNumérica(vX)
+
+# Plota o gráfico
+plt.legend(["Função"])
+plt.axhline(0, color="black", linewidth=0.5)
+plt.plot(vX, vY)
+plt.show()
