5a5ea784685b9f724e7d57b887c8ced6e31056e4
Silas Henrique <silash35@gmail.com>
2024-06-08 01:17:49 -0300

    add project Tomorrow

---
+++ UFBA/LLMs/LLMs/chat.py
@@ -0,0 +1,78 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from youtube_transcript_api import YouTubeTranscriptApi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_link_info(url):
+    if url.startswith("https://www.youtube.com/watch?v="):
+        video_id = url.split("=")[1]
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["pt-BR"])
+        lines = [line["text"] for line in transcript]
+        return "\n".join(lines)  # converto a lista de strings para uma única string
+
+    loader = PyPDFLoader(url)
+    pages = loader.load()  # devolve um Document, envelopando a string
+    pages = [page.page_content for page in pages]  # tiro a string de dentro do document
+    return "\n".join(pages)  # converto a lista de strings para uma única string
+
+
+model = ChatOpenAI(model="gpt-4o")
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Quero que você responda usando apenas o conhecimento obtido pelas informações disponibilizadas abaixo.",
+        ),
+        ("user", "{user_query}"),
+        (
+            "system",
+            "Informações disponibilizadas: {context_info}",
+        ),
+        (
+            "system",
+            "Responda em até 60 palavras. Caso o usuário pergunte algo fora do contexto das informações disponibilizadas, responda que não sabe.",
+        ),
+    ]
+)
+
+chain = prompt | model
+
+link = input("Cole aqui um link de um video do youtube ou de um pdf: ")
+userPrompt = input("Faça sua pergunta ou requisição sobre o conteúdo do link:")
+
+response = chain.invoke(
+    {
+        "user_query": userPrompt,
+        "context_info": get_link_info(link),
+    }
+)
+
+print(response.content)
+
+
+# Análise de resultados:
+
+# link: https://www.youtube.com/watch?v=Dld8WBqAH2g
+# userPrompt: Faça um resumo do material que eu disponibilizei.
+# Resposta do gpt-4o:
+#   Para se manter o mais seco possível em um dia chuvoso, você deve se mover rapidamente.
+#   A quantidade de chuva que cai sobre você é constante, independentemente da velocidade, mas ao reduzir o tempo na chuva, você minimiza a água recebida na cabeça.
+#   Portanto, mova-se rápido para sair da chuva.
+# Resposta do gpt-3.5-turbo:
+#   O texto explica que, ao tentar se manter o mais seco possível em dias chuvosos, o ideal é sair da chuva o mais rápido possível, pois a quantidade de água que cai sobre a cabeça é constante, independente da velocidade de deslocamento.
+#   Correr ou andar não faz diferença nesse aspecto.
+
+# link: https://s3.novatec.com.br/sumarios/sumario-9788575224144.pdf
+# userPrompt: O que é um Commit?
+# Resposta do gpt-4o:
+#   Um commit é uma ação no Git que salva as alterações feitas no código em um repositório.
+#   Ele captura o estado atual do projeto, permitindo que você acompanhe o histórico de mudanças e colabore com outros desenvolvedores.
+# Resposta do gpt-3.5-turbo:
+#   Commit é a ação de salvar as alterações feitas em um repositório Git.
+#   Ao realizar um commit, as modificações são registradas e associadas a uma mensagem que descreve as mudanças realizadas.
+#   Cada commit representa um ponto na história do projeto e permite rastrear as modificações ao longo do tempo.
---
+++ UFBA/LLMs/LLMs/main.py
@@ -0,0 +1,122 @@
+from langchain_huggingface import HuggingFaceEndpoint
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", task="text-generation")  # type: ignore
+
+questions = [
+    """Código limpo refere-se a um estilo de programação que enfatiza a clareza, legibilidade e manutenibilidade do código fonte.
+Cite 5 benefícios de se utilizar o Clean Code (código limpo) no desenvolvimento de software.
+Utilize tópicos curtos.""",
+    """Como algum desses benefícios pode melhor o seguinte código?
+Mostre como ele poderia ser melhorado utilizando algum dos benefícios listados. Utilize apenas 1 benefício.
+
+def calc_average(scores):
+    total = 0
+    count = 0
+    for score in scores:
+        total = total + score
+        count = count + 1
+    average = total / count
+    return average
+
+grades = [90, 85, 95, 87, 92]
+print("The average grade is:", calc_average(grades))
+""",
+]
+
+history = ""
+
+for question in questions:
+    prompt = f"""<|begin_of_text|>
+
+<|start_header_id|>system<|end_header_id|>
+Você é um assistente especialista em programação.
+Você vai auxiliar o usuário respondendo as perguntas dele e ajudando ele nas tarefas que ele pedir.
+Sempre responda o que ele pedir.
+<|eot_id|>
+
+{history}
+
+<|start_header_id|>user<|end_header_id|>
+{question}
+<|eot_id|>
+
+<|start_header_id|>assistant<|end_header_id|>
+"""
+
+    response = model.invoke(prompt)
+
+    # Adicione a pergunta e a resposta ao histórico
+    history += f"""
+<|start_header_id|>user<|end_header_id|>
+{question}
+<|eot_id|>
+"""
+
+    history += f"""
+<|start_header_id|>assistant<|end_header_id|>
+{response}
+<|eot_id|>
+"""
+
+    # Imprima a pergunta e a resposta
+    print("User: " + question)
+    print("Assistant: " + response)
+
+
+# Conversa com o assistente na integra:
+
+# User: Código limpo refere-se a um estilo de programação que enfatiza a clareza, legibilidade e manutenibilidade do código fonte.
+# Cite 5 benefícios de se utilizar o Clean Code (código limpo) no desenvolvimento de software.
+# Utilize tópicos curtos.
+# Assistant: Aqui estão 5 benefícios de se utilizar o Clean Code no desenvolvimento de software:
+
+# • **Legibilidade**: Código fácil de ler e entender, o que facilita a manutenção e o desenvolvimento do software.
+
+# • **Manutenção**: Código limpo é mais fácil de manter e atualizar, pois sua estrutura e organização são claras e coerentes.
+
+# • **Desenvolvimento**: Código limpo ajuda a desenvolver software de forma mais eficiente, pois os desenvolvedores podem entender melhor o que está sendo feito e trabalhar em equipe de forma mais eficaz.
+
+# • **Redução de bugs**: Código limpo é mais fácil de testar e debugar, pois sua estrutura e lógica são claras e fáceis de entender.
+
+# • **Melhora da produtividade**: Código limpo permite que os desenvolvedores trabalhem mais rapidamente e eficazmente, pois não precisam se preocupar com a complexidade e a confusão do código.
+# User: Como algum desses benefícios pode melhor o seguinte código?
+# Mostre como ele poderia ser melhorado utilizando algum dos benefícios listados. Utilize apenas 1 benefício.
+
+# def calc_average(scores):
+#     total = 0
+#     count = 0
+#     for score in scores:
+#         total = total + score
+#         count = count + 1
+#     average = total / count
+#     return average
+
+# grades = [90, 85, 95, 87, 92]
+# print("The average grade is:", calc_average(grades))
+
+# Assistant: Um exemplo de código que pode ser melhorado utilizando o benefício da "Legibilidade".
+
+# Aqui está o código melhorado:
+# ```
+# def calculate_average(scores):
+#     """
+#     Calculate the average score from a list of scores.
+#     :param scores: A list of scores
+#     :return: The average score
+#     """
+#     return sum(scores) / len(scores)
+
+# grades = [90, 85, 95, 87, 92]
+# print("The average grade is:", calculate_average(grades))
+# ```
+# Nesse exemplo, o código foi melhorado da seguinte forma:
+
+# * O nome da função foi alterado para `calculate_average`, tornando-o mais descritivo e fácil de entender.
+# * A função `sum` e `len` foram utilizadas em vez de variáveis `total` e `count`, tornando o código mais conciso e fácil de ler.
+# * A função agora tem um comentário que descreve o que ela faz, tornando-a mais fácil de entender para outros desenvolvedores.
+
+# Essas alterações melhoraram a legibilidade do código, tornando-o mais fácil de entender e manter.
