1be2e0b8e7ad37694059d9b9bce15994cf46ad93
Silas Henrique <silash35@gmail.com>
2024-11-09 19:38:19 -0300

    improve graphics

--- UFBA/ENGG11/src/plot.py
+++ UFBA/ENGG11/src/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import plotly.graph_objects as go
 import seaborn as sns
 
@@ -7,7 +8,7 @@ from typings import Population
 save_folder = "../media/"
 default_dpi = 96
 
-plt.rcParams.update({"font.size": 12})
+plt.rcParams.update({"font.size": 15})
 
 
 def save_or_show(filename: str | None = None):
@@ -36,7 +37,7 @@ def plot_tanks(t, tanks, labels=None, filename: str | None = None, scatter=0):
 
 
 def plot_space(Cv1, Cv2, Z, filename: str | None = None):
-    plt.figure(figsize=(16, 9), layout="constrained", dpi=default_dpi)
+    plt.figure(figsize=(16 * 0.75, 9 * 0.75), layout="constrained", dpi=default_dpi)
     plt.contourf(Cv1, Cv2, Z ** (1 / 8), levels=200, alpha=1)
     plt.colorbar()
     plt.xlabel("$Cv_1$")
@@ -47,7 +48,7 @@ def plot_space(Cv1, Cv2, Z, filename: str | None = None):
 
 def plot_space_3d(Cv1, Cv2, Z, filename: str | None = None):
     fig = go.Figure(
-        data=[go.Surface(z=Z ** (1 / 8), x=Cv1, y=Cv2)],
+        data=[go.Surface(z=Z ** (1 / 8), x=Cv1, y=Cv2, colorscale="Viridis")],
     )
     fig.update_traces(showscale=False)
     fig.update_layout(
@@ -68,7 +69,7 @@ def plot_space_3d(Cv1, Cv2, Z, filename: str | None = None):
 
 
 def plot_population(Cv1, Cv2, Z, population: Population, filename: str | None = None):
-    plt.figure(figsize=(16, 9), layout="constrained", dpi=default_dpi)
+    plt.figure(figsize=(16 * 0.75, 9 * 0.75), layout="constrained", dpi=default_dpi)
     plt.contourf(Cv1, Cv2, Z ** (1 / 8), levels=200, alpha=0.5, antialiased=True)
 
     plt.xlabel("$Cv_1$")
@@ -78,16 +79,24 @@ def plot_population(Cv1, Cv2, Z, population: Population, filename: str | None = 
     population = [ind for ind in population if ind["fitness"] != best_ind["fitness"]]
 
     for ind in population:
-        plt.scatter(ind["chromosome"][0], ind["chromosome"][1], color="red")
+        plt.scatter(ind["chromosome"][0], ind["chromosome"][1], color="black")
 
-    plt.scatter(best_ind["chromosome"][0], best_ind["chromosome"][1], color="green")
+    plt.scatter(
+        best_ind["chromosome"][0],
+        best_ind["chromosome"][1],
+        marker="*",
+        color="gold",
+        linewidths=1.5,
+        edgecolor="black",
+        s=384,
+    )
 
     save_or_show(filename)
 
 
 def plot_density(values, labels, metric, filename: str | None = None):
-    plt.figure(figsize=(10, 5))
-
+    plt.figure(figsize=(10, 4), layout="constrained", dpi=default_dpi)
+    colors = ["tab:blue", "tab:orange", "tab:green", "tab:red"]
     for i in range(len(values)):
         sns.histplot(
             values[i],
@@ -95,10 +104,19 @@ def plot_density(values, labels, metric, filename: str | None = None):
             label=labels[i],
             stat="density",
             bins=15,
+            color=colors[i],
+        )
+
+        mean_value = np.mean(values[i])
+        plt.axvline(
+            mean_value,
+            linestyle="--",
+            linewidth=1.5,
+            color=colors[i],
         )
 
     plt.xlabel(metric)
-    plt.ylabel("Densidade")
+    plt.ylabel("Densidade de Probabilidade")
     plt.legend()
 
     save_or_show(filename)
