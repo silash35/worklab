539f7464cedd136f7011e1984f7f5c66775385ae
Silas Henrique <silash35@gmail.com>
2024-11-30 23:51:09 -0300

    Add VLP

--- UFBA/ENGD06/src/plots.py
+++ UFBA/ENGD06/src/plots.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 
 
-def plot_density(values, label, unit):
+def plot_density(values, label, unit, unit_first=False):
     mean_value = np.mean(values)
     sns.histplot(
         values,
@@ -17,7 +17,7 @@ def plot_density(values, label, unit):
         linestyle="--",
         linewidth=1.5,
         color="tab:blue",
-        label=f"Média: {mean_value:.2f}{unit}",
+        label=f"Média: {unit if unit_first else ""}{mean_value:.2f}{"" if unit_first else unit}",
     )
 
     plt.xlabel(f"{label} ({unit})")
